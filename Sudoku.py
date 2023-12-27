4ca3d0450cd14805941c0be0834a1a5dddad9a95
HematAbadi <yaldahmtabdi81@gmail.com>
2023-12-27 10:19:33 +0330

    initializing

---
+++ Sudoku.py
@@ -0,0 +1,21 @@
+class Sudoku():
+    def __init__():
+        pass
+    
+    def available_pos(self, grid):
+        pass
+    
+    def start_element(self, grid):
+        pass
+    
+    def solve(self, grid):
+        pass
+    
+def main():
+    solve_sudoku = Sudoku()
+    
+
+if __name__ == '__main__':
+    main()
+    
+        
